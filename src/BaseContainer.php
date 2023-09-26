8fe4e319933b99de8e299a0d1b20bb5fe4e22df5
Marcin Orlowski <MarcinOrlowski@users.noreply.github.com>
2023-09-26 14:33:33 +0200

    Removed ArrayAccessTrait

--- src/BaseContainer.php
+++ src/BaseContainer.php
@@ -11,20 +11,65 @@ declare(strict_types=1);
 
 namespace MarcinOrlowski\TextTable;
 
-use MarcinOrlowski\TextTable\Traits\ArrayableTrait;
-use MarcinOrlowski\TextTable\Traits\ArrayAccessTrait;
 use MarcinOrlowski\TextTable\Traits\CountableTrait;
 use MarcinOrlowski\TextTable\Traits\IteratorAggregateTrait;
 
 abstract class BaseContainer extends \Lombok\Helper implements ContainerContract
 {
-    use ArrayAccessTrait;
-    use ArrayableTrait;
     use CountableTrait;
     use IteratorAggregateTrait;
+
+    /* **[ ArrayableContract ]******************************************************************* */
 
     public function toArray(): array
     {
         return $this->container->toArray();
+    }
+
+    /* **[ ArrayAccess ]************************************************************************* */
+
+    public function offsetExists(mixed $offset): bool
+    {
+        /** @var string|int $offset */
+        return $this->container instanceof \ArrayAccess
+            ? $this->container->offsetExists($offset)
+            : \array_key_exists($offset, $this->container);
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        /** @var string|int $offset */
+        return $this->container instanceof \ArrayAccess
+            ? $this->container->offsetGet($offset)
+            : $this->container[$offset];
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @throws \InvalidArgumentException
+     */
+    public function offsetSet(mixed $offset, mixed $value): void
+    {
+        if ($this->container instanceof \ArrayAccess) {
+            $this->container->offsetSet($offset, $value);
+        } else {
+            if ($offset === null) {
+                $this->container[] = $value;
+            } else {
+                /** @var string|int $offset */
+                $this->container[$offset] = $value;
+            }
+        }
+    }
+
+    public function offsetUnset(mixed $offset): void
+    {
+        /** @var string|int $offset */
+        if ($this->container instanceof \ArrayAccess) {
+            $this->container->offsetUnset($offset);
+        } else {
+            unset($this->container[$offset]);
+        }
     }
 }
